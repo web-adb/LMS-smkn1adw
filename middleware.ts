0285885f75e6847b4d374ca28954d6f66a5ad69d
web-adb <web@smkn1adw.sch.id>
2024-09-30 08:40:00 +0700

    update some config

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     // Routes that can be accessed while signed out
-    publicRoutes: ['/api/uploadthing', '/api/webhook'],
-    // Routes that can always be accessed, and have
-    // no authentication information
-    ignoredRoutes: ['/no-auth-in-this-route'],
+    publicRoutes: ['/api/uploadthing', '/api/webhook'], // Ensure webhook is accessible
+    ignoredRoutes: ['/no-auth-in-this-route'], // Add other routes as needed
 });
 
 export const config = {
-    // Protects all routes, including api/trpc.
-    // See https://clerk.com/docs/references/nextjs/auth-middleware
-    // for more information about configuring your Middleware
     matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
